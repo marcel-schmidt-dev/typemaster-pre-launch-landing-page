56ea123b9924163d88bc8d29ea047da4417445e6
Marcel Schmidt <marcelschmidt1989@live.de>
2024-06-14 13:44:04 +0200

    added mobile

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var d = new Date();
 year.innerHTML = d.getFullYear();
 
-// Definieren der Medienabfragen
 const mobileQuery = window.matchMedia('(max-width: 375px)');
 const tabletQuery = window.matchMedia('(max-width: 768px)');
 
-// Funktion zum Überprüfen der Medienabfragen und Zurückgeben des Gerätetyps
 function getDeviceType() {
     if (mobileQuery.matches) {
         return 'mobile';
@@ -16,18 +14,14 @@ function getDeviceType() {
     }
 }
 
-// Funktion zum Umgang mit Änderungen der Medienabfrage
 function handleMediaQueryChange() {
     const deviceType = getDeviceType();
     imgKeyboard.src = './assets/' + deviceType + '/image-keyboard.jpg';
     imgPhoneKeyboard.src = './assets/' + deviceType + '/image-phone-and-keyboard.jpg';
     imgGlassKeyboard.src = './assets/' + deviceType + '/image-glass-and-keyboard.jpg';
-    console.log(`Current device type: ${deviceType}`);
 }
 
-// Listener für Änderungen der Medienabfragen hinzufügen
 mobileQuery.addEventListener('change', handleMediaQueryChange);
 tabletQuery.addEventListener('change', handleMediaQueryChange);
 
-// Initiale Überprüfung
 handleMediaQueryChange();
